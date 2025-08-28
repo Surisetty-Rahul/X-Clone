b2c497647ea2d7781c7f10a89fa68f64f2c740d5
Surisetty-Rahul <rahulsurisetty06@gmail.com>
2025-08-28 15:33:10 +0530

    3-payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //middleware
-app.use(express.json()); // to parse req.body
+app.use(express.json({ limit: "5mb" }));  // to parse req.body
+//limit should not be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse urlencoded data
 
 app.use(cookieParser()); // to parse cookies
